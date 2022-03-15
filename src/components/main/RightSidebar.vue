54e2ae20868189f153dac94b585f278dee7ce8a7
shoaibuddin12fx <shoaibuddin12fx@gmail.com>
2022-03-15 18:01:30 +0500

    images set

--- src/components/main/RightSidebar.vue
+++ src/components/main/RightSidebar.vue
@@ -1,53 +1,77 @@
 <template>
-    <div class="sidebar">
-        <h1>Hello</h1>
-        <select @change="updateChartType($event)" >
-            <option v-for="type of types" :key="type">{{type}}</option>
-        </select>
-        <button @click="sendPush()">PRESS</button>
-    </div>
+  <div class="sidebar">
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
 </template>
 <script>
-
 export default {
-    name: 'RightSidebar',
-    data() {
-        return {
-            types: [
-                'line',
-                'area',
-                'bar',
-                'radar',
-                'histogram',
-                'pie',
-                'donut',
-                'radialBar',
-                'scatter',
-                'bubble',
-                'heatmap',
-                'candlestick',
-            ],
-            key: "box"
-        }
-    },
-    methods: {
-        sendPush(){
-            this.emitter.emit("apex-obj", {
-                key: "value"
-            });
+  name: "RightSidebar",
+  data() {
+    return {
+      options: {
+        chart: {
+          animations: {
+            enabled: true,
+            easing: "easeinout",
+            speed: 800,
+            animateGradually: {
+              enabled: true,
+              delay: 150,
+            },
+            dynamicAnimation: {
+              enabled: true,
+              speed: 350,
+            },
+          },
+          
         },
-        updateChartType($event){
-            let v = $event.target.value;
-            if(v){
-                this.emitter.emit("apex-type-change", {
-                    type: v
-                });
-            }
-            
-        }
-    }
-}
+      },
+      types: [
+        "line",
+        "area",
+        "bar",
+        "radar",
+        "histogram",
+        "pie",
+        "donut",
+        "radialBar",
+        "scatter",
+        "bubble",
+        "heatmap",
+      ],
+      key: "box",
+    };
+  },
+  methods: {
+    sendPush() {
+      this.emitter.emit("apex-obj", {
+        key: "value",
+      });
+    },
+    updateChartType($event) {
+      let v = $event.target.value;
+      if (v) {
+        this.emitter.emit("apex-type-change", {
+          type: v,
+        });
+      }
+    },
+  },
+};
 </script>
 <style scoped>
-
 </style>
